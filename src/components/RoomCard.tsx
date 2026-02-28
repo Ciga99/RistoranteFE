320c571f0acbbd260bd1404d6dbee4cd0fc4fea5
Ciga <61182064+Ciga99@users.noreply.github.com>
2026-02-28 11:38:38 +0100

    FIx button arrow

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -19,9 +19,9 @@ export default function RoomCard({room}: {room: RoomType}) {
         <Card>
             <div className="flex flex-col sm:flex-row cursor-pointer gap-4">
                 <div className="relative flex items-center justify-center group">
-                    <ArrowBigLeft className="text-black absolute left-0 z-10 p-1 bg-white/50 rounded-full hover:bg-white transition-colors" onClick={handlePrevImg} />
+                    {room.listOfImages.length > 1 && <ArrowBigLeft className="text-black absolute left-0 z-10 p-1 bg-white/50 rounded-full hover:bg-white transition-colors" onClick={handlePrevImg} />}
                     <img className="h-56 w-full sm:h-48 sm:w-64 lg:h-64 lg:w-80 xl:h-72 xl:w-96 object-cover rounded-md flex-shrink-0" src={room.listOfImages[currentIndexImg]} alt={room.name} />
-                    <ArrowBigRight className="text-black absolute right-0 z-10 p-1 bg-white/50 rounded-full hover:bg-white transition-colors" onClick={handleNextImg}/>
+                    {room.listOfImages.length > 1 && <ArrowBigRight className="text-black absolute right-0 z-10 p-1 bg-white/50 rounded-full hover:bg-white transition-colors" onClick={handleNextImg}/>}
                 </div> 
                 <div className="flex flex-col gap-2">
                     <h2 className="text-2xl font-bold text-gray-700">{room.name}</h2>
