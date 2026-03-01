5719576733a645d3cdfb1c281cd13f13a9277bf2
Ciga <61182064+Ciga99@users.noreply.github.com>
2026-03-01 11:18:13 +0100

    TODO

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,12 @@ function App() {
 }
 
 export default App
-//TODO - Sistemare  hgero mettre tutto in un unvoc componnte 
 //TODO - aggiungere calendario per dispondibilita camere
 //TODO - npm i react-calendar calednmario per la prenotazione camera 
 //TODO - prenotazione tavolo con calendario e orari disponibili
 //TODO in caso per i pagamenti si puo usrare Stripe 
+//TODO - aggiungere sezione admin per gestire menu e camere (CRUD)
+//TODO - aggiungere informatica sui cookies e sulla privacy (GDPR)
+//TODO - aggiungere la multilingua (italiano e inglese) con react-i18next
+//TODO - aggiungere Agggiuntrer topografia almeno al titotlo in alto 
+//TODO - aggiungere animaziononi con framer-motion 
